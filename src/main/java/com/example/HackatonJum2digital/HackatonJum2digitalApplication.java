6f74209c35180abdebf8fcdcdac3bc4134af69ea
Abdessamad belkhialat <abde.ripo@gmail.com>
2023-10-17 11:56:40 +0200

    First commit

---
+++ src/main/java/com/example/HackatonJum2digital/HackatonJum2digitalApplication.java
@@ -0,0 +1,13 @@
+package com.example.HackatonJum2digital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackatonJum2digitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackatonJum2digitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HackatonJum2digital/HackatonJum2digitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HackatonJum2digital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackatonJum2digitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
